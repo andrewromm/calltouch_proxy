13efec45143238d405a917f1bc3819bc7d8fdf97
Andrey Romm <andreyromm@Andreys-MacBook-Air.local>
2022-09-14 18:45:49 +0300

    init

--- server.py
+++ server.py
@@ -2,11 +2,34 @@ from aiohttp import web
 import logging
 
 
-async def index(request):
-    print("HEADERS:", request.headers)
-    post_data = await request.post()
-    print("POST:", post_data)
-    return web.Response(text="Welcome home!")
+token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJtZWRhbHZpYW4iLCJuYW1lIjoiRnVja1lvdSIsImlhdCI6MTUxNjIzOTAyMn0.0wgLveTkc5tbyjmwltvBDMy4XSyII5HCMnx0iKKRnbE"
+
+class CalltouchEntry():
+    def __init__(self, phone_number, request_url, session_id, fio="", email="", comment="") -> None:
+        self.fio = fio
+        self.phoneNumber = phone_number
+        self.email = email
+        self.subject = "Заявка с сайта"
+        self.comment = comment
+        self.requestUrl = request_url
+        self.sessionId = session_id 
+
+
+async def index(self):
+    if self.headers["token_ct"] != token:
+        print("Incerrect TOKEN")
+        return web.Response(status=500)
+    post_data = await self.post()
+    ct_entry = CalltouchEntry(
+        phone_number=post_data["phone"] if "phone" in post_data else "", 
+        request_url=self.headers["Referer"],
+        session_id=post_data["_ct_session_id"] if "_ct_session_id" in post_data else "",
+        fio=post_data["name"] if "name" in post_data else "",
+        email=post_data["email"] if "email" in post_data else "",
+        comment=post_data["comments"] if "comments" in post_data else ""
+    )
+    print("CT_ENTRY:", ct_entry.__dict__)
+    return web.Response(status=200)
 
 
 if __name__ == '__main__':
