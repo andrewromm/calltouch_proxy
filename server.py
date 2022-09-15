192b9119a96211e1b46abca8a13671c39335c6ef
Andrey Romm <andreyromm@Andreys-MacBook-Air.local>
2022-09-15 09:24:34 +0300

    init

--- server.py
+++ server.py
@@ -2,11 +2,13 @@ from aiohttp import web, ClientSession
 from aiojobs.aiohttp import setup, spawn
 import logging
 import re
+import json
 
 
 TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJtZWRhbHZpYW4iLCJuYW1lIjoiRnVja1lvdSIsImlhdCI6MTUxNjIzOTAyMn0.0wgLveTkc5tbyjmwltvBDMy4XSyII5HCMnx0iKKRnbE"
 CT_MEDALVIAN_ID = "3295"
 CT_CMZMEDICAL_ID = "16474"
+logger = None
 
 class CalltouchEntry():
     def __init__(self, phone_number, request_url, session_id, fio="", email="", comment="") -> None:
@@ -21,13 +23,16 @@ class CalltouchEntry():
 
 
 async def index(request):
-    if "token_ct" not in request.headers or request.headers["token_ct"] != TOKEN:
-        print("Incorrect TOKEN")
-        return web.Response(status=500)
     post_data = await request.post()
-    print("POST from site:", post_data)
+    referer = request.headers["Referer"] if "Referer" in request.headers else ""
+    
+    if "token_ct" not in request.headers or request.headers["token_ct"] != TOKEN:
+        logger.error(f"Incorrect TOKEN from {referer}")
+        return web.Response(status=500)
+    
+    logger.debug(f"POST from {referer}: {json.dumps(post_data)}")
     session_id = re.search(r"_ct_session_id=\d*", post_data["COOKIES"]) if "COOKIES" in post_data else None
-    ct_entry = CalltouchEntry(phone_number=post_data["phone"] if "phone" in post_data else "", request_url=request.headers["Referer"] if "Referer" in request.headers else "", session_id=session_id[0][15:] if session_id is not None else "", fio=post_data["name"] if "name" in post_data else "", email=post_data["email"] if "email" in post_data else "", comment=post_data["comments"] if "comments" in post_data else "")
+    ct_entry = CalltouchEntry(phone_number=post_data["phone"] if "phone" in post_data else "", request_url=referer, session_id=session_id[0][15:] if session_id is not None else "", fio=post_data["name"] if "name" in post_data else "", email=post_data["email"] if "email" in post_data else "", comment=post_data["comments"] if "comments" in post_data else "")
 
     await spawn(request, push_to_calltouch(ct_entry))
     
@@ -50,11 +55,18 @@ async def push_to_calltouch(ct_entry: CalltouchEntry):
             ) as resp:
                 resp = await resp.json(encoding="utf8")
                 if "requestId" in resp:
-                    print(f'CT request was created at {resp["dateStr"]}, id {resp["requestId"]}')
+                    logger.debug(f'CT request was created at {resp["dateStr"]}, id {resp["requestId"]}')
 
 
 if __name__ == '__main__':
-    logging.basicConfig(level=logging.ERROR)
+    logging.basicConfig(
+        filename="log.log",
+        filemode='a',
+        format='%(asctime)s,%(msecs)d %(name)s %(levelname)s %(message)s',
+        datefmt='%H:%M:%S',
+        level=logging.DEBUG)
+    logger = logging.getLogger()
+
     server = web.Application()
     server.router.add_post('/', index)
 
